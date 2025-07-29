74dfa46809bb62fb396ad80895da5659d6a16d5e
DWraith23 <bwilson5924@gmail.com>
2025-07-28 22:10:38 -0400

    Added a load canvas option from the start menu.

--- DrawingSystem/StartMenu.cs
+++ DrawingSystem/StartMenu.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 
 namespace DrawingSystem;
 
@@ -15,6 +16,7 @@ public partial class StartMenu : Control
     private VBoxContainer Contents => GetNode<VBoxContainer>("CenterContainer/Panel/Contents");
     private Button NewDrawingButton => Contents.GetNode<Button>("New Drawing");
     private Button LoadPictureButton => Contents.GetNode<Button>("Load Picture");
+    private Button LoadCanvasButton => Contents.GetNode<Button>("Load Canvas");
     private Button ExitButton => Contents.GetNode<Button>("Exit");
 
     public override void _Ready()
@@ -26,6 +28,7 @@ public partial class StartMenu : Control
     {
         NewDrawingButton.Pressed += OnNewDrawingButtonPressed;
         LoadPictureButton.Pressed += OnLoadPicturePressed;
+        LoadCanvasButton.Pressed += OnLoadCanvasPressed;
         ExitButton.Pressed += OnExitButtonPressed;
     }
 
@@ -64,7 +67,36 @@ public partial class StartMenu : Control
             AddChild(popup);
             popup.Popup();
         }
-        
+    }
+
+    private void OnLoadCanvasPressed()
+    {
+        var dialog = new FileDialog()
+        {
+            FileMode = FileDialog.FileModeEnum.OpenFile,
+            Access = FileDialog.AccessEnum.Filesystem,
+            Filters = ["*.res"],
+            Title = "Load Canvas",
+            InitialPosition = Window.WindowInitialPosition.CenterMainWindowScreen,
+            Size = new Vector2I(500, 400),
+        };
+        dialog.FileSelected += OnCanvasSelectedForLoad;
+        AddChild(dialog);
+        dialog.Popup();
+    }
+
+    private void OnCanvasSelectedForLoad(string path)
+    {
+        var resource = ResourceLoader.Load(path);
+        if (resource is not CanvasDetails canvas) return;
+        if (canvas.Canvas == null) return;
+
+        var scene = canvas.Canvas.Instantiate<Canvas>();
+        if (canvas.Drawings != null) scene.DrawingSystem.Drawings = [.. canvas.Drawings];
+        var root = GetTree().Root;
+        root.GetChildren().ToList().ForEach(child => child.QueueFree());
+
+        root.AddChild(scene);
     }
 
     private void OnExitButtonPressed() => GetTree().Quit();
